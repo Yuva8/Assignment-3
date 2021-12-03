4d782bc4b504f5246038c382e442a2cd1343c81c
Yuva8 <95079980+Yuva8@users.noreply.github.com>
2021-12-03 20:40:39 +0530

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,16 @@
+var object = new XMLHttpRequest();
+
+object.open('GET', 'http://api.countrylayer.com/v2/currency/EUR?access_key=275509dcaede36f110349896cd8ac3e7');
+
+object.send();
+
+object.onload = function(){
+    var data = JSON.parse(this.response);
+    //console.log(data)
+    for (i=0;i<data.length;i++){
+        console.log(data[i].name)
+    }
+   
+
+}
+
